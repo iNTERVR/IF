73590d0aedf9e15341ed76d0cef5b50f712ff0aa
ckbang <ckbang7@gmail.com>
2020-08-18 04:57:32 +0900

    change binding systems in there package sample

--- Runtime/Bootstrap/IF_ApplicationBehaviour.cs
+++ Runtime/Bootstrap/IF_ApplicationBehaviour.cs
@@ -9,27 +9,6 @@ namespace InterVR.IF
     [DefaultExecutionOrder(-20000)]
     public abstract class IF_ApplicationBehaviour : EcsRxApplicationBehaviour
     {
-        protected override void BindSystems()
-        {
-            base.BindSystems();
-
-            Container.BindApplicableSystems(
-                "InterVR.IF.Systems",
-                "InterVR.IF.ViewResolvers");
-            Container.BindApplicableSystems(
-                "InterVR.IF.VR.Systems",
-                "InterVR.IF.VR.ViewResolvers");
-            Container.BindApplicableSystems(
-                "InterVR.IF.VR.Glove.Systems",
-                "InterVR.IF.VR.Glove.ViewResolvers");
-            Container.BindApplicableSystems(
-                "InterVR.IF.VR.Plugin.Steam.Systems",
-                "InterVR.IF.VR.Plugin.Steam.ViewResolvers");
-            Container.BindApplicableSystems(
-                "InterVR.IF.VR.Glove.Plugin.SteamVRManus.Systems",
-                "InterVR.IF.VR.Glove.Plugin.SteamVRManus.ViewResolvers");
-        }
-
         protected override void ApplicationStarted()
         {
             this.EventSystem.Publish(new IF_ApplicationStartedEvent() { });
